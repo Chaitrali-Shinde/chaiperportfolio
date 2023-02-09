0d1ac082a735f32dc96988dc55859bc78aac729b
Chaitrali Shinde <chai.shinde100@gmail.com>
2023-02-08 18:30:40 -0700

    contact page

--- pages/Resume.tsx
+++ pages/Resume.tsx
@@ -1,60 +1,92 @@
 import Link from "next/link";
 import React from "react";  
 import NavBar from "../components/NavBar";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';    
+
+const Item = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(1),
+    textAlign: 'center',
+  }));
 
 export default function Header() {
     return (
         <header>
             <NavBar />
-            <div className="resume-section">
-            <h2>EDUCATION</h2>
-            <div className="resume-education">
-                <h3>Master of Information Technology</h3>
-                <p>Expected December 2024</p>
-                <p>Arizona State University</p>
-                <p>Tempe, AZ, USA</p>
-                <p>Courses: Data visualization and reporting in IT, Data in cloud for IT, Info Systems Development</p>
+            <Grid container spacing={2} paddingTop={10}>
+                
+            <Grid xs={6} md={5} display="flex" justifyContent="center" alignItems="center">
+            <div >
+              <div>
+                EDUCATION
+              </div> 
             </div>
-            <div className="resume-education">
-                <h3>Bachelor of Computer Engineering</h3>
-                <p>December 2021</p>
-                <p>Savitribai Phule Pune University</p>
-                <p>Pune, Maharashtra, India</p>
-                <p>Courses: Data Structures and algorithms, Object-oriented programming, Computer networks, Operating systems, Database management system, Design and analysis of algorithms, Web technology, Data analytics</p>
+          </Grid>
+          <Grid xs={6} md={6} display="flex" justifyContent="center" alignItems="center">
+            <Grid spacing={2} >
+
+              <div>
+              Arizona State University
+              </div>
+              <div>
+              Master of Information Technology (Expected Graduation: May 2024)
+              </div>
+              <div>
+              Courses: Data visualization and reporting in IT, Data in cloud for IT, Info Systems Development</div>
+              <div>
+              Savitribai Phule Pune University
+              </div>
+              <div>
+              Bachelor of Computer Engineering (2017 - 2021)
+              </div>
+              <div>
+             Courses: Data Structures and algorithms, Object-oriented programming, Computer networks, Operating systems, Database management system, Design and analysis of algorithms, Web technology, Data analytics</div>
+            </Grid>
+          </Grid>
+          <Grid xs={6} md={5} display="flex" justifyContent="center" alignItems="center">
+            <div >
+              <div
+               >
+                TECHNICAL SKILLS
+              </div> 
             </div>
+          </Grid>
+          <Grid xs={6} md={6} display="flex" justifyContent="center" alignItems="center">
+            <Grid spacing={2} >
+              <div>
+              Programming Languages: Java, Python, C, C++, JavaScript, SQL, MongoDB
+              </div>
+              <div>
+              Framework/Libraries: Bootstrap, NumPy, Pandas, React JS, Spring Boot, Bokeh
+              </div>
+              <div>
+              Tools, Databases, and OS, Platform: MySQL, Git, Windows, MacOS, Linux/Unix, Docker, AWS, Azure, GCP, Tableau              </div>
+            </Grid>
+          </Grid>
+          <Grid xs={6} md={5} display="flex" justifyContent="center" alignItems="center">
+            <div >
+              <div
+               >
+                TECHNICAL SKILLS
+              </div> 
             </div>
-            <div className="resume-section">
-                <h2>TECHNICAL SKILLS</h2>
-                <p>Programming Languages: Java, Python, C, C++, JavaScript, SQL, MongoDB</p>
-                <p>Framework/Libraries: Bootstrap, NumPy, Pandas, React JS, Spring Boot, Bokeh</p>
-                <p>Tools, Databases, and OS, Platform: MySQL, Git, Windows, MacOS, Linux/Unix, Docker, AWS, Azure, GCP, Tableau</p>
-            </div>
-            <div className="resume-section">
-            <h2>PROFESSIONAL EXPERIENCE</h2>
-            <div className="resume-work-experience">
-                <h3>Engineer</h3>
-                <p>June 2021 - July 2022</p>
-                <p>Tata Consultancy Services (TCS)</p>
-                <p>
-                Provided SaaS solutions to more than 15 customers on various cloud service platforms such as AWS, Azure, and GCP for cloud migration
-                <br/>Developed a python script for automation of manual pricing efforts, boosted pricing productivity
-                <br/>Led a team of 3 for three months to revise cloud services choice according to product types, increased the SaaS 
-                solution's competency
-                <br/>Researched and documented AWS and Azure services according to various business use-cases, enhance 
-                efficiency of services provided
-                </p>
-                <h3>Intern</h3>
-                <p>August 2020 - March 2021</p>
-                <p>BOS Global India Private Limited</p>
-                <p>
-                PDeveloped an application to capture screen time and monitor employee's activities during work hours
-                <br/>Reduced managing efforts by 50% by developing a web application with data analysis dashboard
-                <br/>Built window's installer in C programing language, improved installer's effectiveness by 70%
-                <br/>Programmed windows installer and web application using C, advance installer, React JS, spring boot, MySQL, 
-                and followed agile conventions
-                </p>
-            </div>
-            </div>
+          </Grid>
+          <Grid xs={6} md={6} display="flex" justifyContent="center" alignItems="center">
+            <Grid spacing={2} >
+              <div>
+              Programming Languages: Java, Python, C, C++, JavaScript, SQL, MongoDB
+              </div>
+              <div>
+              Framework/Libraries: Bootstrap, NumPy, Pandas, React JS, Spring Boot, Bokeh
+              </div>
+              <div>
+              Tools, Databases, and OS, Platform: MySQL, Git, Windows, MacOS, Linux/Unix, Docker, AWS, Azure, GCP, Tableau              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+           
         </header>
     )
 }
